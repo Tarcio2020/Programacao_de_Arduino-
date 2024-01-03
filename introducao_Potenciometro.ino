993790158013d534ab279be9333beda581da2571
Tarcio Teles da Silva Farias <tarcioapps7@gmail.com>
2024-01-02 21:22:57 -0300

    update and delete

---
+++ introducao_Potenciometro.ino
@@ -0,0 +1,14 @@
+// C++ code
+//
+int pot;//variável para o potenciometro
+void setup()
+{
+  Serial.begin(9600); //liga o monitor serial
+}
+
+void loop()
+{
+  pot = analogRead(A1); //potenciomentro na entrada analógica 1
+  Serial.println(pot); //printar na tela os dados do potenciometro que vão de 0 até 1023 são 1024 valores
+  delay(1);
+}
